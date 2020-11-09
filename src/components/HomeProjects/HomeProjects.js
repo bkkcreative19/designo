166e43a9904afb2d5a649e8368b3e3e273e198c2
Kyle King <bkkcreative16@gmail.com>
2020-11-09 15:00:48 -0600

    finished home projects section

---
+++ src/components/HomeProjects/HomeProjects.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./HomeProjects.scss";
+
+const HomeProjects = () => {
+  return (
+    <section className="home-projects container">
+      <div className="web-design">
+        <h1>Web Design</h1>
+        <p>View Projects</p>
+      </div>
+      <div className="app-design">
+        <h1>App Design</h1>
+        <p>View Projects</p>
+      </div>
+      <div className="graphic-design">
+        <h1>Graphic Design</h1>
+        <p>View Projects</p>
+      </div>
+    </section>
+  );
+};
+
+export default HomeProjects;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import Hero from '../../components/Hero/Hero'
-import './Home.scss'
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import HomeProjects from "../../components/HomeProjects/HomeProjects";
+import "./Home.scss";
 
 const Home = () => {
-    return (
-        <div className='home'>
-            <Hero/>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <Hero />
+      <HomeProjects />
+    </div>
+  );
+};
 
-export default Home
+export default Home;

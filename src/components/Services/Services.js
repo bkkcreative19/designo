6b1688ea39746c5711af1ee9596de425d2cb70c7
Kyle King <bkkcreative16@gmail.com>
2020-11-09 15:34:27 -0600

    finished services section

---
+++ src/components/Services/Services.js
@@ -0,0 +1,50 @@
+import React from "react";
+import passionate from "../../assets/home/desktop/illustration-passionate.svg";
+import resourceful from "../../assets/home/desktop/illustration-resourceful.svg";
+import friendly from "../../assets/home/desktop/illustration-friendly.svg";
+import "./Services.scss";
+
+const Services = () => {
+  return (
+    <section className="services container">
+      <div className="services__item passionate">
+        <div className="services__item-img-container">
+          <img src={passionate} />
+        </div>
+
+        <h2>Passionate</h2>
+        <p>
+          Each project starts with an in-depth brand research to ensure we only
+          create products that serve a purpose. We merge art, design, and
+          technology into exciting new solutions.
+        </p>
+      </div>
+      <div className="services__item resourceful">
+        <div className="services__item-img-container">
+          <img src={resourceful} />
+        </div>
+
+        <h2>Resourceful</h2>
+        <p>
+          Everything that we do has a strategic purpose. We use an agile
+          approach in all of our projects and value customer collaboration. It
+          guarantees superior results that fulfill our clients’ needs.
+        </p>
+      </div>
+      <div className="services__item friendly">
+        <div className="services__item-img-container">
+          <img src={friendly} />
+        </div>
+
+        <h2>Friendly</h2>
+        <p>
+          We are a group of enthusiastic folks who know how to put people first.
+          Our success depends on our customers, and we strive to give them the
+          best experience a company can provide.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import HomeProjects from "../../components/HomeProjects/HomeProjects";
+import Services from "../../components/Services/Services";
 import "./Home.scss";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div className="home">
       <Hero />
       <HomeProjects />
+      <Services />
     </div>
   );
 };

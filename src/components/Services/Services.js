ba7d1bc7062bd04f5a9ae98f1719141fd5fdd334
Kyle King <bkkcreative16@gmail.com>
2020-11-10 10:50:48 -0600

    finished footer

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -12,36 +12,42 @@ const Services = () => {
           <img src={passionate} />
         </div>
 
-        <h2>Passionate</h2>
-        <p>
-          Each project starts with an in-depth brand research to ensure we only
-          create products that serve a purpose. We merge art, design, and
-          technology into exciting new solutions.
-        </p>
+        <div className="services__item-text">
+          <h2>Passionate</h2>
+          <p>
+            Each project starts with an in-depth brand research to ensure we
+            only create products that serve a purpose. We merge art, design, and
+            technology into exciting new solutions.
+          </p>
+        </div>
       </div>
       <div className="services__item resourceful">
         <div className="services__item-img-container">
           <img src={resourceful} />
         </div>
 
-        <h2>Resourceful</h2>
-        <p>
-          Everything that we do has a strategic purpose. We use an agile
-          approach in all of our projects and value customer collaboration. It
-          guarantees superior results that fulfill our clients’ needs.
-        </p>
+        <div className="services__item-text">
+          <h2>Resourceful</h2>
+          <p>
+            Everything that we do has a strategic purpose. We use an agile
+            approach in all of our projects and value customer collaboration. It
+            guarantees superior results that fulfill our clients’ needs.
+          </p>
+        </div>
       </div>
       <div className="services__item friendly">
         <div className="services__item-img-container">
           <img src={friendly} />
         </div>
 
-        <h2>Friendly</h2>
-        <p>
-          We are a group of enthusiastic folks who know how to put people first.
-          Our success depends on our customers, and we strive to give them the
-          best experience a company can provide.
-        </p>
+        <div className="services__item-text">
+          <h2>Friendly</h2>
+          <p>
+            We are a group of enthusiastic folks who know how to put people
+            first. Our success depends on our customers, and we strive to give
+            them the best experience a company can provide.
+          </p>
+        </div>
       </div>
     </section>
   );
